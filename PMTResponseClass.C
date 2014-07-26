191b2f769c3c2c60ab2013254ba8c9c786e5f3d3
Chris Anelli <Christopher.Anelli@gmail.com>
2014-07-26 07:12:09 -0400

    Can plot individual Gaussians on the same canvas.

--- PMTResponseClass.C
+++ PMTResponseClass.C
@@ -34,43 +34,47 @@ const double ThermalFraction=0.383; //Fraction of Thermal Noise
 const double ThermalParameter=0.034; //Thermal Noise Parameter
 
 
-
 class PMTResponseObject {
+  int n, numPEPeaks;
 public:
-  int n;
-  //PMTResponseObject();
-  double Signal(double *x, double *p);  
-  double Signal()(double *x, double *p){
-   return Signal(x,p);
-      }
+  PMTResponseObject();
+  //double Signal(double *x, double *p);  
+  //double Signal()(double *x, double *p){
+  //return Signal(x,p);
+  //}
+  void setN(int i){
+    n = i;
+  }
+  int getN(){
+    return n;
+  }
   double PhotoElectron_N(double *x, double *p);
   
-  double PhotoElectron_N()(double *x, double *p){
-    return PhotoElectron_N(x,p);
-  }
+  //double PhotoElectron_N()(double *x, double *p){
+  //  return PhotoElectron_N(x,p);
+  //}
   
   //double testFunction()(double *x, double*p){
   //  return n* testFunction(x,p);
   //}
 };
 
+
 /*
  * Constructor
  */
-/*
 PMTResponseObject::PMTResponseObject(){
-  n=2;
+  n=1;
   numPEPeaks=2;
 }
-*/
+
 /*
  * Function For Signal, Sums Over PhotoElectron Series
  */
 double PMTResponseObject::Signal(double *x, double *par){
   double signal = 0; // Initialize/Clear to 0;
   cout << numPEPeaks;
-  for (n=0; n < numPEPeaks; n++){
-    
+  for (n=0; n < numPEPeaks; n++){    
     signal += PhotoElectron_N(x,par);
   }
   n = 0; //Set Back to Default
@@ -87,28 +91,42 @@ double PMTResponseObject::PhotoElectron_N(double *x, double *par){
   n==0 ? Sigma_n =par[4] : Sigma_n = sqrt(n)*par[2]; 
   // cout << Sigma_n << endl;
   double f = TMath::PoissonI(n,par[0])*(1-par[5])*TMath::Gaus(xx,Q_n, Sigma_n, kTRUE);
+  //double f = TMath::Gaus(xx, Q_n, Sigma_n, kTRUE);
+  //double f = TMath::Gaus(xx, n*par[1], par[2], kTRUE);
   return f;
 }
 
 void DrawPMTResponse(){  
   cout << "working" << endl;
-  PMTResponseObject* fObj = new PMTResponseObject();
-  fObj->n=2;
+  
+  //cout << fObj->getN();
+  //fObj->n=2;
+  //cout << fObj->n<< endl;
   //TF1 *f1 = new TF1("f1", fObj,&PMTResponseObject::Signal, 0, 300, 6, "PMTResponseObject", "Signal");
-  TF1 *f1 = new TF1("f1", fObj,&PMTResponseObject::PhotoElectron_N, 0, 300, 3, "PMTResponseObject", "PhotoElectron_N");
   
-  //fObj->numPEPeaks=1;
-  //cout << fObj->numPEPeaks << endl;
+
+  for(int i =0; i< 4; i++){
+    PMTResponseObject* fObj = new PMTResponseObject();  //Class Object is strongly linked to TF1.
+    fObj->setN(i);
+    cout << fObj->getN() << endl;
+    TF1 *f2 = new TF1(Form("f2_%i",i), fObj,&PMTResponseObject::PhotoElectron_N,
+		      0, 300, 6, 
+		      "PMTResponseObject", "PhotoElectron_N");
   
-  //TF1 *f1 = new TF1("f1", PMTResponseObject::testFunction, 0, 10, 1);
-  f1->SetParameter(0,mu);
-  f1->SetParameter(1,SinglePE);
-  f1->SetParameter(2,SinglePESpread);
-  //f1->SetParameter(3,Pedestal);
-  //f1->SetParameter(4,PedestalSpread);
-  //f1->SetParameter(5,ThermalFraction);
-  f1->Draw();
+    //TF1 *f1 = new TF1("f2", PMTResponseObject::testFunction, 0, 10, 1);
+    f2->SetParameter(0,mu);
+    f2->SetParameter(1,SinglePE);
+    f2->SetParameter(2,SinglePESpread);
+    f2->SetParameter(3,Pedestal);
+    f2->SetParameter(4,PedestalSpread);
+    f2->SetParameter(5,ThermalFraction);
+    f2->SetNpx(1000); //Number of Points to Draw
+    if(i==0) f2->Draw();
+    if(i!=0) f2->Draw("same");
+  }
   //string PMTResponse = DefinePMTResponseFunction();
   //cout << PMTResponse << endl;
   //test();
 }
+
+
